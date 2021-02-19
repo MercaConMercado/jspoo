d1e76355c053f67e084573a31b3861ddd285cd06
MercaConMercado <decodermc@gmail.com>
2021-02-19 07:45:39 -0500

    js poo v2

--- index.js
+++ index.js
@@ -16,7 +16,9 @@ class datos {
 
 /**
 * metodo metodos 
-* @returns {void}
+* @type {validarClave()}
+* @returns {respuesta}
+* 
 */
 class metodos {
     validarClave() {
@@ -37,14 +39,14 @@ class metodos {
 }
 
 // UI
-usuario = prompt('usuario')
-clave1 = prompt('clave1')
-clave2 = prompt('clave2')
+let usuario = prompt('usuario')
+let clave1 = prompt('clave1')
+let clave2 = prompt('clave2')
 
-//setear clase de (datos) con contrusctor de los datos
+//setear clase de (datos) con contrusctor de los datos apartir de UI
 const Objeto = new datos(usuario, clave1, clave1)
 console.log(Objeto)
 
-//setear clase de (metodos) con metodo (validarClave()
+//setear clase de (metodos) con metodo (validarClave() para las operaciones
 let vali = metodos.prototype.validarClave(toString.respuesta)
 console.log(vali)
